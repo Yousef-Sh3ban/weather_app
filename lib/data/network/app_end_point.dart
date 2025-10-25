830650f2f982a0ca21ef0f41aba526286dfeeeb0
Yousef-Sh3ban <wewewe1551@gmail.com>
2025-10-25 10:03:24 +0300

    make readme file and add screenshots

--- lib/data/network/app_end_point.dart
+++ lib/data/network/app_end_point.dart
@@ -2,6 +2,6 @@ abstract class AppEndPoint {
   static String baseUrl="http://api.weatherapi.com/v1/";
   static String forecastUrl="${baseUrl}forecast.json";
   static String searchUrl="${baseUrl}search.json";
-  static String apiKey="dc416aaf0ab14d4aa8f191815252801";
+  static String apiKey="YOUR_API_KEY_HERE";
 
 }
